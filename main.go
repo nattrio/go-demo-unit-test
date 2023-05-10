6f4285e7bcc6f10a7b7859a580eed57d54e5991e
Jirapat Klaokliang <jirapat.nattrio@gmail.com>
2023-05-10 22:54:28 +0700

    test(main): mock CustomerRepository

--- main.go
+++ main.go
@@ -1,4 +1,32 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+
+	"github.com/stretchr/testify/mock"
+)
+
 func main() {
+	// Create an instance of our test struct
+	c := CustomerRepositoryMock{}
+	c.On("GetCustomer", 1).Return("John", 20, nil)
+	c.On("GetCustomer", 2).Return("Mary", 30, errors.New("not found"))
+
+	name, age, err := c.GetCustomer(1)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(name, age)
+}
+
+type CustomerRepositoryMock struct {
+	mock.Mock
+}
+
+// GetCustomer is a mock function for CustomerRepository.GetCustomer
+func (m *CustomerRepositoryMock) GetCustomer(id int) (name string, age int, err error) {
+	args := m.Called(id)
+	return args.String(0), args.Int(1), args.Error(2) // 0 is the first argument, 1 is the second, and so on...
 }
