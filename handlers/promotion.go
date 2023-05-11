d4aee1a27cf46beaae49ebd397d34e91bb4bfefa
Jirapat Klaokliang <jirapat.nattrio@gmail.com>
2023-05-11 10:44:28 +0700

    feat(handler): Create promotion handler with fiber

---
+++ handlers/promotion.go
@@ -0,0 +1,38 @@
+package handlers
+
+import (
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/nattrio/go-demo-unit-test/services"
+)
+
+type PromotionHandler interface {
+	CalculateDiscount(c *fiber.Ctx) error
+}
+
+type promotionHandler struct {
+	promoService services.PromotionService
+}
+
+// NewPromotionHandler is a constructor for promotionHandler
+func NewPromotionHandler(promoService services.PromotionService) PromotionHandler {
+	return promotionHandler{promoService: promoService} // return a new instance of promotionHandler
+}
+
+func (h promotionHandler) CalculateDiscount(c *fiber.Ctx) error {
+	// http://localhost:8000/calculatediscount?amount=100
+
+	amountStr := c.Query("amount")
+	amount, err := strconv.Atoi(amountStr)
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+
+	discount, err := h.promoService.CalculateDiscount(amount)
+	if err != nil {
+		return c.SendStatus(fiber.StatusNotFound)
+	}
+
+	return c.SendString(strconv.Itoa(discount))
+}
